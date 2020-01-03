020c63a0ab9697a4c3b90705a5142e66b0c3ae59
RECNEPS-KIPS <2432719656@qq.com>
2020-01-03 22:08:16 +0800

    增添了FSM类
    FSM用于存放判断参数和统一调用各状态机

---
+++ Assets/Scripts/FSM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM : MonoBehaviour
+{
+    public IState currentState { get; private set; }
+    public bool isCanAttack;
+    public bool isCanMove;
+    public PlayerController playerController;
+    void Start() {
+
+    }
+
+    public void ChangeState(IState newState) {
+        currentState = newState;
+    }
+
+    void Update() {
+        if (currentState != null && playerController != null) {
+            currentState.Execute(playerController);
+        }
+    }
+}
